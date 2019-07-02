cc19af7eb85c7db4267ce0e89525eafdcc2d9cbc
MafeCarbo <mcarbonells@unal.edu.co>
2019-07-02 10:42:18 -0500

    Example Documentation

--- Code/Ahorcado/app/src/main/java/com/DataAcces/Repositories/UserRepository.java
+++ Code/Ahorcado/app/src/main/java/com/DataAcces/Repositories/UserRepository.java
@@ -8,9 +8,15 @@ public class UserRepository {
 
     Context context;
 
-    public void create (User user){}
+    /**
+     *
+     * @param user
+     * @return
+     */
+    public boolean create (User user){return true;}
     public void delete (String email_user){}
     public void update (String email_user, short score){}
+
     public User getbyEmail(){
         return null;
     }
