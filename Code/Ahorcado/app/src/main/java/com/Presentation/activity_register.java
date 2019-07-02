623bcc46eb950602db443b6ea9e7e1a59b8dbea1
samoralespu <samoralespu@unal.edu.co>
2019-07-02 10:45:47 -0500

    activity register and login

---
+++ Code/Ahorcado/app/src/main/java/com/Presentation/activity_register.java
@@ -0,0 +1,13 @@
+package com.Presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
