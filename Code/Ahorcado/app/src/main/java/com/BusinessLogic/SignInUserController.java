5b5d2a0d55d43d33060703b35c0d8512ae2608b7
DanielaDuque <30605041+DanielaDuque@users.noreply.github.com>
2019-07-03 19:42:12 -0500

    refactor

--- Code/Ahorcado/app/src/main/java/com/BusinessLogic/SignInUserController.java
+++ Code/Ahorcado/app/src/main/java/com/BusinessLogic/SignInUserController.java
@@ -6,9 +6,15 @@ import android.content.Context;
 import com.DataAcces.Models.User;
 import com.DataAcces.Repositories.UserRepository;
 
-public class SignInController {
+public class SignInUserController {
     Context context;
 
+    /**
+     * Esta funcion registra un usuario en la applicacion, haciendo llamado a la clase UserRepository.
+     * @param email email del usuario a registrar
+     * @param password password del usuario a registar.
+     * @return
+     */
     public boolean singin(String email, String password){
         User user = new User(email,password,0);
         UserRepository userRepository = new UserRepository (context);
