121b27ff28d647866347c71f56df06a3ca5e5b6c
samoralespu <samoralespu@unal.edu.co>
2019-07-06 13:45:51 -0500

    Checkbox funcionando

--- Code/Ahorcado/app/src/main/java/com/BusinessLogic/LoginAdminController.java
+++ Code/Ahorcado/app/src/main/java/com/BusinessLogic/LoginAdminController.java
@@ -8,6 +8,12 @@ public class LoginAdminController {
     public LoginAdminController(Context context) {
         this.context = context;
     }
+    /**
+     * Esta funcion loguea un administrador en la applicacion, haciendo llamado a la clase AdminRepository.
+     * @param email email del administrador registrado
+     * @param password password del administrador registrado.
+     * @return
+     */
     public boolean loginAdmin(String email, String password){
         return true;
     }
