8d349141e5ff50a01bf7bdec17eaf91aee06da49
samoralespu <samoralespu@unal.edu.co>
2019-07-06 15:34:55 -0500

    Add play, word, category activities

---
+++ Code/Ahorcado/app/src/main/java/com/Presentation/PlayActivity.java
@@ -0,0 +1,14 @@
+package com.Presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ahorcado.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
