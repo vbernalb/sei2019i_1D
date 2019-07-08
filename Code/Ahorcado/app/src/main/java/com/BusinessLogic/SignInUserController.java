c725512c19348a3688871d400811b479eadd1941
MafeCarbo <mcarbonells@unal.edu.co>
2019-07-08 12:09:19 -0500

    ,

--- Code/Ahorcado/app/src/main/java/com/BusinessLogic/SignInUserController.java
+++ Code/Ahorcado/app/src/main/java/com/BusinessLogic/SignInUserController.java
@@ -20,26 +20,24 @@ public class SignInUserController {
      * @param password password del usuario a registar.
      * @return
      */
-    public boolean singin(String email, String password, String password2){
-        if(this.PasswordValidation(password, password2)&& this.EmailValidation(email)&& this.UserExist(email)){
+    public void singin(String email, String password, String password2){
+        if(this.PasswordValidation(password, password2)&& this.EmailValidation(email)){
             User user = new User(email, password, 0);
             userRepository.create(user, "http://ahorcado1d.000webhostapp.com/insert_user.php");
-            return true;
+            this.userExist1(email);
         }
-        else return false;
     }
 
     private boolean PasswordValidation(String password, String password2){
         return password.equals(password2);
     }
     private boolean EmailValidation(String email){
-        return email.contains("@unal.edu.co")|| email.contains("@hotmail.com")||email.contains("@gmail.com")
-                || email.contains("@outlook.com");
+        return email.contains("@");
     }
-    public  void UserExist1(String email){
-        userRepository
+    private  void userExist1(String email){
+        userRepository.getbyEmail("http://ahorcado1d.000webhostapp.com/get_user.php", email, null, 2);
     }
-    public static boolean userExist (User user){
-        return true;
+    public static void userExist (User user){
+
     }
 }
