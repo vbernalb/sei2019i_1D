ac486d61cc9d3481e11a7d296b5664b2c23ea7f7
MafeCarbo <mcarbonells@unal.edu.co>
2019-07-09 10:47:13 -0500

    c

--- Code/Ahorcado/app/src/main/java/com/BusinessLogic/InsertCategoryController.java
+++ Code/Ahorcado/app/src/main/java/com/BusinessLogic/InsertCategoryController.java
@@ -6,10 +6,11 @@ import com.DataAcces.Models.Category;
 import com.DataAcces.Models.User;
 import com.DataAcces.Repositories.CategoryRepository;
 import com.DataAcces.Repositories.UserRepository;
+import com.Presentation.CategoryActivity;
+import com.Presentation.RegisterActivity;
 
 public class InsertCategoryController {
     Context context;
-    static boolean exist;
     CategoryRepository categoryRepository;
 
     public InsertCategoryController(Context context) {
@@ -17,18 +18,19 @@ public class InsertCategoryController {
         this.categoryRepository= new CategoryRepository(context);
     }
 
-    public boolean InsertCategory (String name_category){
-        Category category = new Category(name_category);
-        if(exist){
-            categoryRepository.create(category, "http://ahorcado1d.000webhostapp.com/insert_category.php");
-            return true;
-        }
-        else return false;
+    public void InsertCategory (String name_category){
+         categoryExist(name_category);
     }
     private void categoryExist (String name_category){
         categoryRepository.getbyCategory("http://ahorcado1d.000webhostapp.com/get_category.php", name_category);
     }
-    public static void categoryExist1(Category category){
-        exist= category!=null;
+    public  void categoryExist1(Category category, String name_category){
+        boolean confirm =false;
+        final CategoryActivity ca= (CategoryActivity) context;
+        if(category!= null){
+            confirm=true;
+            categoryRepository.create(new Category(name_category), "http://ahorcado1d.000webhostapp.com/insert_category.php");
+        }
+
     }
 }
