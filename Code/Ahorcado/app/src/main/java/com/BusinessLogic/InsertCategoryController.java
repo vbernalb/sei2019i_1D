ee7e795a2449ae7cec7b5bb534a76096bb7f48c4
Vale <vbernalb@unal.edu.co>
2019-07-20 16:45:37 -0500

    cambios

--- Code/Ahorcado/app/src/main/java/com/BusinessLogic/InsertCategoryController.java
+++ Code/Ahorcado/app/src/main/java/com/BusinessLogic/InsertCategoryController.java
@@ -3,6 +3,7 @@ package com.BusinessLogic;
 import android.content.Context;
 
 import com.DataAcces.Models.Category;
+import com.DataAcces.Models.Difficulty_Category;
 import com.DataAcces.Models.User;
 import com.DataAcces.Repositories.CategoryRepository;
 import com.DataAcces.Repositories.UserRepository;
@@ -29,4 +30,17 @@ public class InsertCategoryController {
         }
         ca.nuevoIntent(confirm, context);
     }
+
+    public  void diff_cat_create(Difficulty_Category dc, int id_diff_cat){
+        boolean confirm =false;
+        final CategoryActivity ca= (CategoryActivity) context;
+        if(dc==null){
+            confirm=true;
+            System.out.println(name_category);
+            new CategoryRepository(context).create(new Category(name_category), "http://ahorcado1d.000webhostapp.com/insert_category.php");
+        }
+        ca.nuevoIntent(confirm, context);
+    }
+
+
 }
