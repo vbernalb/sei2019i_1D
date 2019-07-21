0f371ab1a8905ddf241e5f5bada452c8bccdfd9b
samoralespu <samoralespu@unal.edu.co>
2019-07-21 13:48:15 -0500

    finishActivitie

--- Code/Ahorcado/app/src/main/java/com/Presentation/GameActivity.java
+++ Code/Ahorcado/app/src/main/java/com/Presentation/GameActivity.java
@@ -2,9 +2,11 @@ package com.Presentation;
 
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -28,13 +30,15 @@ public class GameActivity extends AppCompatActivity {
     ImageView img;
     LinearLayout ly;
     private TextView tv;
-    int count=1;
+    int count = 1;
+    public int numOfTrials = 6;
+    public static int time = 6000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        img= (ImageView) findViewById(R.id.image_ahorc);
-        ly= (LinearLayout) findViewById(R.id.layout_word);
+        img = (ImageView) findViewById(R.id.image_ahorc);
+        ly = (LinearLayout) findViewById(R.id.layout_word);
         int WrapWidth = LinearLayout.LayoutParams.WRAP_CONTENT;
         int WrapHeight = LinearLayout.LayoutParams.WRAP_CONTENT;
         String inputString = "";
@@ -45,62 +49,62 @@ public class GameActivity extends AppCompatActivity {
     String resultWord = "--------";
     char[] fromDB = "Ahorcado".toLowerCase().toCharArray();
     boolean isMatch;
-    public int numOfTrials = 6;
 
-    public String getTempWord(String touchString){
+
+    public String getTempWord(String touchString) {
         isMatch = false;
         char touchChar = touchString.toLowerCase().charAt(0);
-        for(int k = 0; k < fromDB.length; k++)
-            if(fromDB[k] == touchChar) {
+        for (int k = 0; k < fromDB.length; k++)
+            if (fromDB[k] == touchChar) {
                 outputWord[k] = touchChar;
                 isMatch = true;
             }
         return new String(outputWord);
     }
 
-    public boolean isMatch(){
+    public boolean isMatch() {
         return isMatch;
     }
 
-    public void setInitWord(String wordFromDB){
+    public void setInitWord(String wordFromDB) {
         TextView guessWord = findViewById(R.id.textView30);
         String initWord = "";
-        for(char c : wordFromDB.toCharArray())
+        for (char c : wordFromDB.toCharArray())
             initWord += "-";
         guessWord.setText(initWord);
     }
     // metodo para verificar, desactivar botones y cambiar color
 
-    public void setState(){
+    public void setState() {
         TextView numOfTrials = findViewById(R.id.textView40);
         String message = "";
 
-        if(isMatch) {
+        if (isMatch) {
             message = "Quedan " + GameActivity.this.numOfTrials + " intentos";
-            if(hasWon()){
+            if (hasWon()) {
                 message = "¡GANASTE!";
-                img.setImageResource(R.drawable.g);
                 deactivateAllButtons();
-                // INSERTAR CODIGO DEBAJO DE ESTE COMENTARIO
+
+                img.setImageResource(R.drawable.g);
+                cambiarActivity(time);
             }
-        }
-        else if(GameActivity.this.numOfTrials > 1 && !isMatch){
+        } else if (GameActivity.this.numOfTrials > 1 && !isMatch) {
             GameActivity.this.numOfTrials--;
             message = "Quedan " + GameActivity.this.numOfTrials + " intentos";
 
-            if(count==1){
+            if (count == 1) {
                 img.setImageResource(R.drawable.a);
             }
-            if(count==2){
+            if (count == 2) {
                 img.setImageResource(R.drawable.b);
             }
-            if(count==3){
+            if (count == 3) {
                 img.setImageResource(R.drawable.c);
             }
-            if(count==4){
+            if (count == 4) {
                 img.setImageResource(R.drawable.d);
             }
-            if(count==5){
+            if (count == 5) {
                 img.setImageResource(R.drawable.e);
             }
             count++;
@@ -111,11 +115,12 @@ public class GameActivity extends AppCompatActivity {
             deactivateAllButtons();
             TextView gameOverWord = findViewById(R.id.textView30);
             gameOverWord.setText(new String(fromDB));
+            cambiarActivity(time);
         }
         numOfTrials.setText(message);
     }
 
-    public void deactivateButtons(LinearLayout inputLinearLayout){
+    public void deactivateButtons(LinearLayout inputLinearLayout) {
         for (int i = 0; i < 9; i++) {
             Button btn = (Button) inputLinearLayout.getChildAt(i);
             btn.setEnabled(false);
@@ -123,7 +128,7 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
-    public void deactivateAllButtons(){
+    public void deactivateAllButtons() {
         LinearLayout linearLayout = findViewById(R.id.linearLayout10);
         deactivateButtons(linearLayout);
         LinearLayout linearLayout2 = findViewById(R.id.linearLayout20);
@@ -141,7 +146,7 @@ public class GameActivity extends AppCompatActivity {
         guessWord.setText(getTempWord(str));
         btn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#F31C0A")));
 
-        if(isMatch)
+        if (isMatch)
             colorButton = "#00F700";
         else
             colorButton = "#F31C0A";
@@ -151,22 +156,33 @@ public class GameActivity extends AppCompatActivity {
         //Toast.makeText(this, "Boton desactivado", Toast.LENGTH_SHORT).show();
     }
 
-    public boolean hasWon(){
+    public boolean hasWon() {
         String fromDBString = new String(fromDB);
         TextView guessWord = findViewById(R.id.textView30);
 
-        if(fromDBString.equals(guessWord.getText().toString()))
+        if (fromDBString.equals(guessWord.getText().toString()))
             return true;
 
         return false;
     }
 
     //cerrar sesion
-    public void exit(View view){
+    public void exit(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
+
+    public void cambiarActivity(int milisegundos) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                final Intent i = new Intent(GameActivity.this, FinishActivity.class);
+                GameActivity.this.startActivity(i);
+                GameActivity.this.finish();
+            }
+        }, milisegundos);
+    }
 
 }
