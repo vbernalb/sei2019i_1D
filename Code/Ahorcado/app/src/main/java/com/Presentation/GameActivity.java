007c0ade85754ef3c0ca869b2e882cb9682bc519
samoralespu <samoralespu@unal.edu.co>
2019-07-20 19:02:13 -0500

    image changed and deleted button

--- Code/Ahorcado/app/src/main/java/com/Presentation/GameActivity.java
+++ Code/Ahorcado/app/src/main/java/com/Presentation/GameActivity.java
@@ -26,72 +26,18 @@ import org.w3c.dom.Text;
 
 public class GameActivity extends AppCompatActivity {
     ImageView img;
-    Button btn;
     LinearLayout ly;
     private TextView tv;
-    int count=0;
+    int count=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        btn= (Button)findViewById(R.id.img_btn);
         img= (ImageView) findViewById(R.id.image_ahorc);
         ly= (LinearLayout) findViewById(R.id.layout_word);
         int WrapWidth = LinearLayout.LayoutParams.WRAP_CONTENT;
         int WrapHeight = LinearLayout.LayoutParams.WRAP_CONTENT;
-        //prueba cambiar imagen
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(count==1){
-                    img.setImageResource(R.drawable.a);
-                }
-                if(count==2){
-                    img.setImageResource(R.drawable.b);
-                }
-                if(count==3){
-                    img.setImageResource(R.drawable.c);
-                }
-                if(count==4){
-                    img.setImageResource(R.drawable.d);
-                }
-                if(count==5){
-                    img.setImageResource(R.drawable.e);
-                }
-                if(count==6){
-                    img.setImageResource(R.drawable.f);
-                }
-                count++;
-            }
-
-
-        });
-        // prueba tamaño palabra
-        //cambiar número probar text view
-
         String inputString = "";
-
-/*       for (int i = 0; i <= 15 ; i++)
-        {
-            TextView myText = new TextView(this);
-            myText.setHeight(100);
-            myText.setWidth(100);
-            myText.setBackground(getDrawable(R.drawable.edittext_form));
-            myText.setText("L"+ i);
-            ly.addView(myText);
-        }
-        */
-
-        /*final int c = 12;
-            final TextView[] mtext = new TextView[c];
-            for (int i = 0; i < c; i++) {
-            TextView rowtxt = new TextView(this);
-            rowtxt.setText("Hello" + i);
-            myLinearLayout.addView(rowtxt);
-            myTextViews[i] = rowtxt;
-            myTextViews[i].setOnClickListener(onclicklistener);*/
-        //TextView guessWord = findViewById(R.id.textView30);
-        //guessWord.setText("--------");
         setInitWord("--------");
     }
 
@@ -99,7 +45,7 @@ public class GameActivity extends AppCompatActivity {
     String resultWord = "--------";
     char[] fromDB = "Ahorcado".toLowerCase().toCharArray();
     boolean isMatch;
-    public int numOfTrials = 5;
+    public int numOfTrials = 6;
 
     public String getTempWord(String touchString){
         isMatch = false;
@@ -128,14 +74,33 @@ public class GameActivity extends AppCompatActivity {
     public void setState(){
         TextView numOfTrials = findViewById(R.id.textView40);
         String message = "";
+
         if(isMatch) {
             message = "Quedan " + GameActivity.this.numOfTrials + " equivocaciones";
         }
         else if(GameActivity.this.numOfTrials > 1 && !isMatch){
             GameActivity.this.numOfTrials--;
             message = "Quedan " + GameActivity.this.numOfTrials + " equivocaciones";
+            if(count==1){
+                img.setImageResource(R.drawable.a);
+            }
+            if(count==2){
+                img.setImageResource(R.drawable.b);
+            }
+            if(count==3){
+                img.setImageResource(R.drawable.c);
+            }
+            if(count==4){
+                img.setImageResource(R.drawable.d);
+            }
+            if(count==5){
+                img.setImageResource(R.drawable.e);
+            }
+            count++;
+
         } else {
             message = "Perdiste";
+            img.setImageResource(R.drawable.f);
             LinearLayout linearLayout = findViewById(R.id.linearLayout10);
             deactivateButtons(linearLayout);
             LinearLayout linearLayout2 = findViewById(R.id.linearLayout20);
