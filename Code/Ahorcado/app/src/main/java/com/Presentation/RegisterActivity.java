90207720b1da0a5c2ba97188a7072fddb59c73ba
samoralespu <samoralespu@unal.edu.co>
2019-07-22 18:20:43 -0500

    hint register activity

--- Code/Ahorcado/app/src/main/java/com/Presentation/RegisterActivity.java
+++ Code/Ahorcado/app/src/main/java/com/Presentation/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.Presentation;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +17,7 @@ import com.example.ahorcado.R;
 public class RegisterActivity extends AppCompatActivity {
     EditText email, password, password2;
     Button signIn;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +28,15 @@ public class RegisterActivity extends AppCompatActivity {
         password=(EditText)findViewById(R.id.Password);
         password2=(EditText)findViewById(R.id.Password2);
         signIn= (Button)findViewById(R.id.SingIn);
-
+        fab = (FloatingActionButton) findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegisterActivity.this, "EL correo debe ser un correo válido (con @)" + "\n"+
+                        "Las contraseñas deben coincidir" + "\n"+
+                        "Las contraseñas deben contener entre 6 y 18 caracteres", Toast.LENGTH_SHORT).show();
+            }
+        });
         final SignInUserController suc= new SignInUserController(RegisterActivity.this);
 
         signIn.setOnClickListener(new View.OnClickListener() {
